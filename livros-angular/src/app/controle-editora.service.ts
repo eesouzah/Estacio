5382b9d5fe508748d7828648253ecbbc8f3775d7
eesouzah <esouzah202@gmail.com>
2025-10-27 23:28:27 -0400

    Trabalhos

---
+++ livros-angular/src/app/controle-editora.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Editora } from './editora';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ControleEditoraService {
+  private editoras: Array<Editora> = [
+    new Editora(1, "Angular LTDA"),
+    new Editora(2, "Est Dados"),
+    new Editora(3, "Mod Web")
+  ];
+
+  getEditoras(): Array<Editora> {
+    return this.editoras;
+  }
+
+  getNomeEditora(codEditora: number): string {
+    const editora = this.editoras.filter(e => e.codEditora === codEditora)[0];
+    return editora ? editora.nome : '';
+  }
+}
